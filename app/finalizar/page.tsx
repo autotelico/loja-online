e0637ce2daacf3a06cec957283adc623ca59215e
Henrique Heron <henriqueheron1@gmail.com>
2024-04-28 23:51:41 -0300

    fix: wrap useSearchParams hook with Suspense

--- app/finalizar/page.tsx
+++ app/finalizar/page.tsx
@@ -1,25 +1,35 @@
-'use client'
-import { useSearchParams } from "next/navigation"
-import { Item } from "../components/ItemList"
-import Link from "next/link"
+'use client';
+import { useSearchParams } from 'next/navigation';
+import { Item } from '../components/ItemList';
+import Link from 'next/link';
+import { Suspense } from 'react';
 
 export default function Checkout(): JSX.Element {
-    const searchParams = useSearchParams()
-    const itensAComprar: Item[] = JSON.parse(searchParams.get('stateCartItems')!)
-    
-    return (
-        <div id="checkout" className="mx-auto">
-            <h1 className="text-3xl font-bold">Finalização</h1>
-            <p>Lista de produtos:</p>
-            <ul>
-                {itensAComprar.map(item => <li key={item.id}>{item.title}</li>)}
-            </ul>
-            <Link className="block w-[200px] text-center cursor-pointer bg-[#f2295b] rounded-xl py-2 px-4 mt-5" href={{
-                pathname: '/',
-                query: {
-                    stateCartItems: searchParams.get('stateCartItems')
-                }
-            }}>Voltar ao Catálogo</Link>
-        </div>
-    )
+  const searchParams = useSearchParams();
+  const itensAComprar: Item[] = JSON.parse(searchParams.get('stateCartItems')!);
+
+  return (
+    <div id="checkout" className="mx-auto">
+      <h1 className="text-3xl font-bold">Finalização</h1>
+      <p>Lista de produtos:</p>
+      <ul>
+        {itensAComprar.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+      <Suspense fallback={<p>Carregando página de finalização...</p>}>
+        <Link
+          className="block w-[200px] text-center cursor-pointer bg-[#f2295b] rounded-xl py-2 px-4 mt-5"
+          href={{
+            pathname: '/',
+            query: {
+              stateCartItems: searchParams.get('stateCartItems'),
+            },
+          }}
+        >
+          Voltar ao Catálogo
+        </Link>
+      </Suspense>
+    </div>
+  );
 }
